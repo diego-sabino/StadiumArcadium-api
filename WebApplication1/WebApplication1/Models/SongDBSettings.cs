b8e84a6999cfe156525f4ddf2a0ddd015c862de6
Diego-Sabino <dbrgamei5@gmail.com>
2023-03-04 01:56:24 -0300

    Add Song model for MongoDB integration

---
+++ WebApplication1/WebApplication1/Models/SongDBSettings.cs
@@ -0,0 +1,8 @@
+namespace WebApplication1.Models;
+
+public class SongDBSettings
+{
+    public string ConnectionString { get; set; } = null;
+    public string DatabaseName { get; set; } = null;
+    public string ProductCollectionName { get; set; } = null;
+}
