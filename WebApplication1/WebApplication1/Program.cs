18c7ad4dc86242deb0ee8dfd69cf2d84fb189ad9
Diego-Sabino <dbrgamei5@gmail.com>
2023-03-04 02:08:30 -0300

    Add SongDB config

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,9 +1,12 @@
 using WebApplication1.Models;
+using WebApplication1.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<SongDBSettings>
     (builder.Configuration.GetSection("DevNetStoreDatabase"));
+
+builder.Services.AddSingleton<SongServices>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
